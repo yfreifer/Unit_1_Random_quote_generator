e444e30e0f93dedcd1a023f8411154582e4227e9
yfreifer <yfreifer@users.noreply.github.com>
2019-03-07 18:07:13 +0000

    Update script.js

--- Tech_degree-master/js/script.js
+++ Tech_degree-master/js/script.js
@@ -1,18 +1,18 @@
 
-var quote = [
+var quotes = [
 {quote:"Hello",source:"Jimmy", citation:"Twitter",year:2018,tag:"slksl"},
 {quote:"I love coding",source:"Bill", citation:"Microsoft",year:2019,tag:"slksl"},
 {quote:"Me too",source:"Zuckerberg", citation:"Facebook",year:2019,tag:"slksl"},
 {quote:"To be or not to be",source:"Shakespeare", citation:"Tudor",year:1592,tag:"slksl"},
-{quote:"I like hot dogs",source:"Cody", citation:"Canteen",year:1999,tag:"slksl"}
-
+{quote:"I like hot dogs",source:"Cody", citation:"Canteen",year:1999,tag:"slksl"},
+{quote:"Kel loves orange soda",source:"Kel", citation:"Kenan & Kel",year:1995,tag:"slksl"}
 ];
 
 //generates a random number from the object array
 
 function getRandomQuote(){
-  let randomNum = Math.floor((Math.random() * (quote.length)));
-  return quote[randomNum]; 
+  let randomNum = Math.floor((Math.random() * (quotes.length)));
+  return quotes[randomNum]; 
 }
  
  //based on the random number this function will generate the quote when called
