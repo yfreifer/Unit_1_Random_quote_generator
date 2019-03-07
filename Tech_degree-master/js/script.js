ebbeeef82a16dc7f4590e634528a6d30d541ae55
yfreifer <yfreifer@users.noreply.github.com>
2019-03-07 07:48:16 +0000

    Tech_unit1_submit
    Adding Tech degree file completed

---
+++ Tech_degree-master/js/script.js
@@ -0,0 +1,48 @@
+
+var quote = [
+{quote:"Hello",source:"Jimmy", citation:"Twitter",year:2018,tag:"slksl"},
+{quote:"I love coding",source:"Bill", citation:"Microsoft",year:2019,tag:"slksl"},
+{quote:"Me too",source:"Zuckerberg", citation:"Facebook",year:2019,tag:"slksl"},
+{quote:"To be or not to be",source:"Shakespeare", citation:"Tudor",year:1592,tag:"slksl"},
+{quote:"I like hot dogs",source:"Cody", citation:"Canteen",year:1999,tag:"slksl"}
+
+];
+
+//generates a random number from the object array
+
+function getRandomQuote(){
+  let randomNum = Math.floor((Math.random() * (quote.length)));
+  return quote[randomNum]; 
+}
+ 
+ //based on the random number this function will generate the quote when called
+function printQuote () {
+  var randomItem= getRandomQuote();
+  var codeHTML  = '';
+  codeHTML  += '<p class="quote">' + randomItem.quote + '</p>';
+  codeHTML  += '<p class="source">' + randomItem.source;
+  
+    if (randomItem.citation !== '' && randomItem.citiation !== null) {
+       codeHTML  +='<span class = "citation">' + randomItem.citation + ' </span>';
+    }
+
+    if (randomItem.year !== '' && randomItem.year !== null) {
+        codeHTML  += '<span class = "year">'+ randomItem.year + '</span>';
+    } 
+
+    if (randomItem.tag !== '' && randomItem.tag !== null) {
+      codeHTML  += '<p class = "tag"> Tags: ' + randomItem.tag + '</p>';
+    }
+
+  codeHTML += '</p>';
+
+  document.getElementById('quote-box').innerHTML = codeHTML ; 
+
+}
+ 
+//timer  
+window.setInterval(printQuote, 30000);
+ 
+//event listener which triggers the printQuote function when click  
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+
